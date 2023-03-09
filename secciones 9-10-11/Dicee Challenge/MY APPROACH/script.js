72e9a67c0f48479d3554b9a6fc8c8875e4cb8cd5
l0ku <gonza1390mc@gmail.com>
2023-03-09 16:59:29 -0300

    Fixed not DRY code

--- secciones 9-10-11/Dicee Challenge/MY APPROACH/script.js
+++ secciones 9-10-11/Dicee Challenge/MY APPROACH/script.js
@@ -12,33 +12,36 @@ header.addEventListener('click',(e)=>{
     let randomNumber1 = Math.trunc(Math.random() * 6) + 1;
     let randomNumber2 = Math.trunc(Math.random() * 6) + 1;
 
-    if(randomNumber1 === 1){
-        dice1.src = './images/dice1.png'
-    } else if(randomNumber1 === 2){
-        dice1.src = './images/dice2.png'
-    } else if(randomNumber1 === 3){
-        dice1.src = './images/dice3.png'
-    } else if(randomNumber1 === 4){
-        dice1.src = './images/dice4.png'
-    } else if(randomNumber1 === 5){
-        dice1.src = './images/dice5.png'
-    } else if(randomNumber1 === 6){
-        dice1.src = './images/dice6.png'
-    }
+    dice1.src = `./images/dice${randomNumber1}.png`;
+    dice2.src = `./images/dice${randomNumber2}.png`;
 
-    if(randomNumber2 === 1){
-        dice2.src = './images/dice1.png'
-    } else if(randomNumber2 === 2){
-        dice2.src = './images/dice2.png'
-    } else if(randomNumber2 === 3){
-        dice2.src = './images/dice3.png'
-    } else if(randomNumber2 === 4){
-        dice2.src = './images/dice4.png'
-    } else if(randomNumber2 === 5){
-        dice2.src = './images/dice5.png'
-    } else if(randomNumber2 === 6){
-        dice2.src = './images/dice6.png'
-    }
+    // if(randomNumber1 === 1){
+    //     dice1.src = './images/dice1.png'
+    // } else if(randomNumber1 === 2){
+    //     dice1.src = './images/dice2.png'
+    // } else if(randomNumber1 === 3){
+    //     dice1.src = './images/dice3.png'
+    // } else if(randomNumber1 === 4){
+    //     dice1.src = './images/dice4.png'
+    // } else if(randomNumber1 === 5){
+    //     dice1.src = './images/dice5.png'
+    // } else if(randomNumber1 === 6){
+    //     dice1.src = './images/dice6.png'
+    // }
+
+    // if(randomNumber2 === 1){
+    //     dice2.src = './images/dice1.png'
+    // } else if(randomNumber2 === 2){
+    //     dice2.src = './images/dice2.png'
+    // } else if(randomNumber2 === 3){
+    //     dice2.src = './images/dice3.png'
+    // } else if(randomNumber2 === 4){
+    //     dice2.src = './images/dice4.png'
+    // } else if(randomNumber2 === 5){
+    //     dice2.src = './images/dice5.png'
+    // } else if(randomNumber2 === 6){
+    //     dice2.src = './images/dice6.png'
+    // }
 
     if(randomNumber1 == randomNumber2){
         winner.textContent = `It's a draw!`
