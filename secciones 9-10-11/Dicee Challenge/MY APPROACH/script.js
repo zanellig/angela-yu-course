fe9c12247a5b71c03caa4dec3a1871220e505f6e
l0ku <gonza1390mc@gmail.com>
2023-03-09 17:05:36 -0300

    Added minor visual improvements

--- secciones 9-10-11/Dicee Challenge/MY APPROACH/script.js
+++ secciones 9-10-11/Dicee Challenge/MY APPROACH/script.js
@@ -15,44 +15,16 @@ header.addEventListener('click',(e)=>{
     dice1.src = `./images/dice${randomNumber1}.png`;
     dice2.src = `./images/dice${randomNumber2}.png`;
 
-    // if(randomNumber1 === 1){
-    //     dice1.src = './images/dice1.png'
-    // } else if(randomNumber1 === 2){
-    //     dice1.src = './images/dice2.png'
-    // } else if(randomNumber1 === 3){
-    //     dice1.src = './images/dice3.png'
-    // } else if(randomNumber1 === 4){
-    //     dice1.src = './images/dice4.png'
-    // } else if(randomNumber1 === 5){
-    //     dice1.src = './images/dice5.png'
-    // } else if(randomNumber1 === 6){
-    //     dice1.src = './images/dice6.png'
-    // }
-
-    // if(randomNumber2 === 1){
-    //     dice2.src = './images/dice1.png'
-    // } else if(randomNumber2 === 2){
-    //     dice2.src = './images/dice2.png'
-    // } else if(randomNumber2 === 3){
-    //     dice2.src = './images/dice3.png'
-    // } else if(randomNumber2 === 4){
-    //     dice2.src = './images/dice4.png'
-    // } else if(randomNumber2 === 5){
-    //     dice2.src = './images/dice5.png'
-    // } else if(randomNumber2 === 6){
-    //     dice2.src = './images/dice6.png'
-    // }
-
     if(randomNumber1 == randomNumber2){
-        winner.textContent = `It's a draw!`
+        winner.textContent = `It's a draw! 🙅‍♂️`
         player1.style.color = '#ece75f'
         player2.style.color = '#ece75f'
     } else if (randomNumber1 > randomNumber2){
-        winner.textContent = `Player one wins!`
+        winner.textContent = `🏆 Player one wins!`
         player1.style.color = '#c1ff72'
         player2.style.color = '#ff0000'
     } else if (randomNumber1 < randomNumber2){
-        winner.textContent = `Player two wins!`
+        winner.textContent = `Player two wins! 🏆`
         player2.style.color = '#c1ff72'
         player1.style.color = '#ff0000'
     }
