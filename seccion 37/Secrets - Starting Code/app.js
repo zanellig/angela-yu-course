c1317ab93a43639108efd6d813d6793f7329747c
l0ku <gonza1390mc@gmail.com>
2023-06-22 14:07:26 -0300

    Create basic level 1 login and password

---
+++ seccion 37/Secrets - Starting Code/app.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// Setup express module
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+const root = __dirname;
+
+const { credentials } = require(`${root}/credentials.js`);
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+main().catch(err => console.log(err));
+
+async function main() {
+	await mongoose.connect(
+		`mongodb+srv://${credentials.user}:${credentials.password}@cluster0.dz2qmae.mongodb.net/secretsDB?retryWrites=true&w=majority`
+	);
+
+	const userSchema = new Schema({
+		email: { type: String, required: true },
+		password: { type: String, required: true },
+	});
+
+	const User = mongoose.model('User', userSchema);
+
+	app.get('/', (req, res) => {
+		res.render('home');
+	});
+
+	app.get('/login', (req, res) => {
+		res.render('login');
+	});
+
+	app.get('/register', (req, res) => {
+		res.render('register');
+	});
+
+	app.post('/register', (req, res) => {
+		const emailInput = req.body.username;
+		const passwordInput = req.body.password;
+
+		const newUser = new User({
+			email: emailInput,
+			password: passwordInput,
+		});
+		User.findOne({ email: emailInput }).then(foundUser => {
+			if (!foundUser) {
+				newUser
+					.save()
+					.catch(err => {
+						console.log(err);
+					})
+					.then(() => {
+						res.render('secrets');
+					});
+			} else {
+				console.log(
+					`The user already exists. Please login with your current password.`
+				);
+				res.redirect('register');
+			}
+		});
+	});
+
+	app.post('/login', (req, res) => {
+		const emailInput = req.body.username;
+		const passwordInput = req.body.password;
+
+		User.findOne({ email: emailInput, password: passwordInput }).then(
+			foundUser => {
+				if (foundUser) {
+					res.render('secrets');
+				} else {
+					console.log(`The username or password is incorrect.`);
+					res.redirect('login');
+				}
+			}
+		);
+	});
+
+	app.get('/logout', (req, res) => {
+		res.render('home');
+	});
+
+	app.listen(port, () => {
+		console.log(`Server started on port ${port}`);
+	});
+}
